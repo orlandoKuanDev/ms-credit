c9bca177e05428485a9d70a71f74b88aa6c0aa69
Orlando Kuan <orlandojose.kuanbecerra@everis.nttdata.com>
2021-08-17 17:32:42 -0500

    Feat add Acquistion DTO

--- src/main/java/com/example/mscredit/model/entities/Acquisition.java
+++ src/main/java/com/example/mscredit/model/entities/Acquisition.java
@@ -1,4 +1,20 @@
 package com.example.mscredit.model.entities;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
 public class Acquisition {
+
+    @Field(name = "initial")
+    private double initial;
+
+    @Field(name = "debt")
+    private double debt;
+
+    @Field(name = "cardNumber")
+    private String cardNumber;
+
 }
